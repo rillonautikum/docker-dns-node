7d8aaf6869e30743a689b9001d0b375618aafa17
Philipp Gaßner <activator112233@gmail.com>
2023-09-26 15:01:19 +0200

    changes

--- spec/parserSpec.js
+++ spec/parserSpec.js
@@ -1,8 +1,26 @@
 import Dockerode from 'dockerode';
 import { getDockerSocket } from '../src/api.js';
 
-describe("Parser Test Suite", function() {
-    it("contains spec with an expectation", function() {
-        expect(true).toBe(true);
-    })
-})
+import extract, { DnsEntry } from '../src/parser.js';
+
+describe("Parser Test Suite", function () {
+    it("converts correctly given labels", function () {
+        // console.log(this);
+        let obj = {
+            "com.docker-dns.enabled": "",
+            "com.docker-dns.domain": "example.net",
+            "com.docker-dns.a.v4-test": "10.0.0.1"
+        };
+
+        let workingObj = DnsEntry.fromObject({
+            zone: "example.net",
+            type: "a",
+            name: "v4-test",
+            target: "10.0.0.1"
+        });
+
+        let converted = extract("example.net", obj);
+        // console.log(converted);
+        expect(converted).toEqual([workingObj]);
+    });
+});
