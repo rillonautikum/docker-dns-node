5885ec8c402521be3b719d2529fb256d2186252e
Philipp Gaßner <activator112233@gmail.com>
2023-10-02 18:02:24 +0200

    continued specs

--- spec/utils.js
+++ spec/utils.js
@@ -1,5 +1,30 @@
+import { getDockerSocket } from "../src/api.js";
+
+let docker = getDockerSocket();
+
 function mockDockerLabels() {}
 
-function createDockerContainer() {
-    
+export async function createDockerEnv() {
+    let network = await createDockerNetwork();
+    return {
+        docker, network
+    }
+}
+
+export async function createDockerContainer(image) {
+    return await docker.createContainer({
+        "Image": "nginx"
+    })
+}
+
+async function createDockerNetwork() {
+    let network = await docker.createNetwork({
+        "Name": "TestNetwork",
+        "Driver": "ipvlan",
+        "Options": {
+            "parent": "eth0"    //fix
+        }
+    });
+
+    return network;
 }
